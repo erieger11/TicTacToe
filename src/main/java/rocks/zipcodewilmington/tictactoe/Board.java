253443e465f85e6c3aa60a8314f8fe05e7e403ef
erieger11 <erieger530@gmail.com>
2024-03-13 15:28:44 -0400

    some foundation

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,35 +4,91 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
-        char [] [] theBoard = new char [3][3];
-        char ch = '1';
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                theBoard[i][j]= ch++;
-            }
-        }
-
-
-
-
-
-
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
+        //Switch()
+//        case 1 : if the value of [0][0] .equals [0][1] && [0][2]
+//            return true
+//            break;
+        // case 2 :  if the value of [1][0] .equals [1][1] && [1][2]
+//            return true
+//            break;
+        //case 3 :   if the value of [2][0] .equals [2][1] && [2][2]
+//            return true
+//            break;
+        //case 4 :  if the value of [0][0] .equals [1][0] && [2][0]
+//            return true
+//            break;
+//        case 5 : if the value of [0][1] .equals [1][1] && [2][1]
+//            return true
+//            break;
+//        case 6 : if the value of [0][2] .equals [1][2] && [2][2]
+//            return true
+//            break;
+//        case 7 : if the value of [0][0] .equals [1][1] && [2][2]
+//            return true
+//            break;
+//        case 8 : if the value of [2][0] .equals [1][1] && [0][2]
+//            return true
+//            break;
+//        default : return false
+
         return null;
     }
 
     public Boolean isInFavorOfO() {
+        //Switch()
+//        case 1 : if the value of [0][0] .equals [0][1] && [0][2]
+//            return true
+//            break;
+       // case 2 :  if the value of [1][0] .equals [1][1] && [1][2]
+//            return true
+//            break;
+        //case 3 :   if the value of [2][0] .equals [2][1] && [2][2]
+//            return true
+//            break;
+        //case 4 :  if the value of [0][0] .equals [1][0] && [2][0]
+//            return true
+//            break;
+//        case 5 : if the value of [0][1] .equals [1][1] && [2][1]
+//            return true
+//            break;
+//        case 6 : if the value of [0][2] .equals [1][2] && [2][2]
+//            return true
+//            break;
+//        case 7 : if the value of [0][0] .equals [1][1] && [2][2]
+//            return true
+//            break;
+//        case 8 : if the value of [2][0] .equals [1][1] && [0][2]
+//            return true
+//            break;
+//        default : return false
+
+
+
         return null;
     }
 
     public Boolean isTie() {
+        //if isInFavorOfX() is false &&  if isInFavorOfO() Assert.assertfalse ???
+
+        //return System.out.println "The game ended in a draw"
+
         return null;
     }
 
     public String getWinner() {
+        //if isInFavorOfX() Assert.assertTRUE = true... System.out.println ("X is the Winner)
+        //else if isInFavorOfO() Assert.assertTRUE = true... System.out.println ("X is the Winner)
+        //else isTie()
+
+
         return null;
     }
 
