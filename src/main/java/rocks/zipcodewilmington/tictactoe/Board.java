ad1bd28bf1acbf803cade315406beeaf3b2f0b18
erieger11 <erieger530@gmail.com>
2024-03-13 13:41:24 -0400

    created the board

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -5,6 +5,19 @@ package rocks.zipcodewilmington.tictactoe;
  */
 public class Board {
     public Board(Character[][] matrix) {
+        char [] [] theBoard = new char [3][3];
+        char ch = '1';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                theBoard[i][j]= ch++;
+            }
+        }
+
+
+
+
+
+
     }
 
     public Boolean isInFavorOfX() {
